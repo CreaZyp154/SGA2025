4b482aed545f4f808872e4a0be9153eb45f3a2f6
CreaZyp154 <cyril@strahm.org>
2025-08-09 02:10:13 +0200

    .

--- Potion/Assets/scrpit/UIPotion.cs
+++ Potion/Assets/scrpit/UIPotion.cs
@@ -3,15 +3,20 @@ using UnityEngine.UI;
 
 public class UIPotion : MonoBehaviour
 {
-    public Image image;
 
-    private void Awake()
-    {
-        image = GetComponent<Image>();
-    }
+    public Potion activePotion;
+    public PotionManager manager;
 
     public void Setup(Potion potion)
     {
+        Image image = GetComponent<Image>();
         image.sprite = potion.image;
+        activePotion = potion;
+    }
+
+    public void ApplyPotion()
+    {
+        manager.Apply(activePotion);
+        transform.parent.gameObject.SetActive(false);
     }
 }
--- Potion/Assets/scrpit/triggiepo.cs
+++ Potion/Assets/scrpit/triggiepo.cs
@@ -7,16 +7,13 @@ using NUnit.Framework.Internal;
 public class triggiepo : MonoBehaviour
 {
     [SerializeField] PotionManager manager;
-    public Canvas pot;
+    public GameObject pot;
      
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        pot.enabled = true;
+        pot.SetActive(true);
+        Destroy(this);
     }
 
-    void OnTriggerExit2D(Collider2D other)
-    {
-        pot.enabled = false; 
-    }
 }
